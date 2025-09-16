0e7ab7872f6ac4fe69ebe67dfcfb662c15d36d56
harshavardhan314 <hharshavardhan974@gmail.com>
2025-09-16 22:01:00 +0530

    .

--- Gemini-2.0/src/Components/Main/Main.jsx
+++ Gemini-2.0/src/Components/Main/Main.jsx
@@ -10,6 +10,39 @@ const Main = () => {
             <img src={assets.user_icon} alt="User" />
 
         </div>
+
+        <div className="main-container">
+
+            <div className="greet">
+                <p><span>Hello,Dev</span></p>
+                <p>How can I help you today?</p>
+
+            </div>
+
+            <div className="cards">
+
+                <div className="card">
+                    <p>Suggest Beautiful Places to explore..</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Briefly summarize the concept : urban planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Brainstrom team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Improve readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+
+            </div>
+        </div>
     </div>
   )
 }
